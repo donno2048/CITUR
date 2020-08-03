2ef2560995ef5dd8ec02eb25a253789d5d478080
donno2048 <61805754+donno2048@users.noreply.github.com>
2020-08-03 16:45:40 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,9 @@
+sudo apt-get update
 sudo apt-get install git-all
 sudo apt-get install mercurial
 sudo apt-get install build-essential
+sudo apt-get install unzip
 wait
 git clone https://github.com/donno2048/Rosehip.git
 cd build/
-set FORCE_UNSAFE_CONFIGURE=1
-env FORCE_UNSAFE_CONFIGURE=1
-export FORCE_UNSAFE_CONFIGURE=1
-wait
-sudo bash ./build.sh
+sudo env FORCE_UNSAFE_CONFIGURE=1 bash ./build.sh
